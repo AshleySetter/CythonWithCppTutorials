ceb7d1b45af95961ff8567a9db6b0c51b38a8393
AshleySetter <ajs3g11@soton.ac.uk>
2017-01-22 13:43:32 +0000

    fixed typo

--- ClassCythonCPP/TestClass.pyx
+++ ClassCythonCPP/TestClass.pyx
@@ -8,7 +8,7 @@ cdef extern from "TestClassC.cpp": # defines the source C++ file
 cdef class CTestClass: # defines a python wrapper to the C++ class
     cdef TestClass* thisptr # thisptr is a pointer that will hold to the instance of the C++ class
     def __cinit__(self, int Dimensionality): # defines the python wrapper class' init function
-        self.thisptr = new TestClass(Dimensionality) # creates an instance of the C++ class and puts allocates the pointer to thisB
+        self.thisptr = new TestClass(Dimensionality) # creates an instance of the C++ class and puts allocates the pointer to this
     def __dealloc__(self): # defines the python wrapper class' deallocation function (python destructor)
         del self.thisptr # destroys the reference to the C++ instance (which calls the C++ class destructor
         
