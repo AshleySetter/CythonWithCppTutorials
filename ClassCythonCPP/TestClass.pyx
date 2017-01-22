073b4c49ed119877138b965570a2200873d3704d
AshleySetter <ajs3g11@soton.ac.uk>
2017-01-22 13:37:03 +0000

    added comments saying what everything in the pyx file does

--- ClassCythonCPP/TestClass.pyx
+++ ClassCythonCPP/TestClass.pyx
@@ -1,14 +1,14 @@
 import cython
 
-cdef extern from "TestClassC.cpp":
-    cdef cppclass TestClass:
-        TestClass(int Dimensionality)
-        int Dimensionality
+cdef extern from "TestClassC.cpp": # defines the source C++ file
+    cdef cppclass TestClass: # says that there is a class defined in the above C++ file called TestClass
+        TestClass(int Dimensionality) # the class has public member function TestClass which takes some arguments
+        int Dimensionality # the class has a public variable that is an integer and is called Dimensionality
 
-cdef class CTestClass:
-    cdef TestClass* thisptr # hold a C++ instance
-    def __cinit__(self, int Dimensionality):
-        self.thisptr = new TestClass(Dimensionality)
-    def __dealloc__(self):
-        del self.thisptr
+cdef class CTestClass: # defines a python wrapper to the C++ class
+    cdef TestClass* thisptr # thisptr is a pointer that will hold to the instance of the C++ class
+    def __cinit__(self, int Dimensionality): # defines the python wrapper class' init function
+        self.thisptr = new TestClass(Dimensionality) # creates an instance of the C++ class and puts allocates the pointer to thisB
+    def __dealloc__(self): # defines the python wrapper class' deallocation function (python destructor)
+        del self.thisptr # destroys the reference to the C++ instance (which calls the C++ class destructor
         
