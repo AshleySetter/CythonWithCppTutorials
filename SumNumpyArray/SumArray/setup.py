638835de4610022860587c7d5824c50e42689d62
Ash Setter <ajs3g11@soton.ac.uk>
2017-11-03 11:58:02 +0000

    changed setup.py file to different method - works with importing numpy

--- SumNumpyArray/SumArray/setup.py
+++ SumNumpyArray/SumArray/setup.py
@@ -1,7 +1,10 @@
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Build import cythonize
+from Cython.Distutils import build_ext as build_pyx
+import numpy 
 
-ext = Extension('SumArray', sources=["SumArray.pyx"], language="c++")
 
-setup(name="SumArray", ext_modules = cythonize([ext]))
+ext = [Extension('SumArray', ['SumArray.pyx'], include_dirs=[numpy.get_include()], extra_compile_args=['-O3'], extra_link_args=['-O3'], language="c++")]
+
+setup(name = 'SumArray', ext_modules=ext, cmdclass = { 'build_ext': build_pyx })
+
--- SumNumpyArray/SumArrayParallel/setupOpenMP.py
+++ SumNumpyArray/SumArrayParallel/setupOpenMP.py
@@ -1,7 +1,10 @@
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Build import cythonize
+from Cython.Distutils import build_ext as build_pyx
+import numpy 
 
-ext = Extension('SumArrayParallel', sources=["SumArrayParallel.pyx"], language="c++", extra_compile_args=['-fopenmp'], extra_link_args=['-lgomp'])
 
-setup(name="SumArrayParallel", ext_modules = cythonize([ext]))
+ext = [Extension('SumArrayParallel', ['SumArrayParallel.pyx'], include_dirs=[numpy.get_include()], extra_compile_args=['-O3', '-fopenmp'], extra_link_args=['-O3', '-lgomp'], language="c++")]
+
+setup(name = 'SumArrayParallel', ext_modules=ext, cmdclass = { 'build_ext': build_pyx })
+
